0f9855ad9637feaea45e4a059379b8cc1b2736b4
Victoriia-Oliinyk <103230316+Victoriia-Oliinyk@users.noreply.github.com>
2022-09-02 21:55:07 +0300

    js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,32 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+// импорт SimpleLightbox
+import SimpleLightbox from "simplelightbox";
+// импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 console.log(galleryItems);
+
+
+const galleryRef = document.querySelector('.gallery')
+
+function createGalleryMarkup(galleryItems) {
+  
+  const galleryMarkup = galleryItems.map(({preview,original,description}) => 
+    `
+      <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+    `
+  ).join('')
+
+  return galleryMarkup
+}
+
+galleryRef.insertAdjacentHTML('beforeend',createGalleryMarkup(galleryItems))
+
+
+let gallery = new SimpleLightbox('.gallery a');
+gallery.options.captionsData = 'alt';
+gallery.options.captionDelay = 250;
